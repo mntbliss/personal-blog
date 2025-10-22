fbf1044327a1e01a6ace8c26013d2c93736b1e40
boundless-bliss <qiqitomodachi@gmail.com>
2025-10-22 11:08:46 +0300

    🌺 | Ditch the old soulless layout 2025

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,39 @@ import { isNullOrEmpty } from "./helpers/parsers.js";
 import Discord from "./utility/discord-client-parser.js";
 import MintyPlayer from "./utility/minty-player.js";
 
+SetCurrentTime();
+setInterval(SetCurrentTime, 1000);
+
+function SetCurrentTime() {
+    const date = new Date();
+    
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    document.getElementById('clock-date').innerText = `${date.getFullYear()}/${month}/${day} | ${hours}:${minutes}:${seconds}` //appendChild(timeNode);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const mintyPlayer = new MintyPlayer('bg-video', 'music-toggle');
 const id = '189184578588508161';
